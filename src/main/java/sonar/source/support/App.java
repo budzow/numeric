3837aa60b4a7c723e5de1d809085c6ef431babf4
Michal Budzowski <michal.budzowski@sonarsource.com>
2020-07-27 16:14:29 +0200

    reduce number of parameters in doSth, add doSthElse method

--- src/main/java/sonar/source/support/App.java
+++ src/main/java/sonar/source/support/App.java
@@ -2,7 +2,11 @@ package sonar.source.support;
 
 public class App 
 {
-    public static void doSth(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13)
+    public static void doSth(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8)
+    {
+    }
+    
+    public static void doSthElse(int arg1) 
     {
     }
 }
