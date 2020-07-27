f6fe20324b56d4f7104ef3c11b48accaf83034ce
Michal Budzowski <michal.budzowski@sonarsource.com>
2020-07-27 16:04:28 +0200

    Add file

---
+++ src/main/java/sonar/source/support/App.java
@@ -0,0 +1,8 @@
+package sonar.source.support;
+
+public class App 
+{
+    public static void doSth(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13)
+    {
+    }
+}
